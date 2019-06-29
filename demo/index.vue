145ee5afc86a47e84d9043d6ae986b41919cdb91
520360xy <yanfront@qq.com>
2019-06-29 13:48:13 +0800

    init

---
+++ demo/index.vue
@@ -0,0 +1,31 @@
+<template>
+	<view class="content">
+		<an-upload-img></an-upload-img>
+	</view>
+</template>
+
+<script>
+	import uniGrid from '@/components/uni-grid/uni-grid.vue'
+	import uniIcon from '@/components/uni-icon/uni-icon.vue'
+	import anUploadImg from '@/components/an-uploadImg/an-uploadImg'
+	
+	export default {
+		components:{
+			anUploadImg
+		},
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+	.content{
+		background-color: #f2f2f2;
+	}
+</style>
